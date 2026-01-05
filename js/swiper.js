161be98f3207a49c16ba72a2ed28f44322b2b5c2
Maisdesign <downloadtaky@gmail.com>
2026-01-05 03:35:55 +0100

    Refactor swiper.js to improve pagination and autoplay settings, and adjust responsive breakpoints

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,33 @@
 var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    770: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    992: {
       slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-    });
+      spaceBetween: 50
+    },
+  },
+  loop: true,
+});
